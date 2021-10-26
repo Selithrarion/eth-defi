b051929cf44846f256bff56a3f8db2b372c4e126
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-10-26 13:46:38 +0500

    add bank withdraw

--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -11,6 +11,7 @@ contract dBank {
   mapping(address => bool) public isDeposited;
 
   event Deposit(address indexed user, uint etherAmount, uint timeStart);
+  event Withdraw(address indexed user, uint etherAmount, uint depositTime, uint interest);
 
   constructor(Token _token) public {
     token = _token;
@@ -28,20 +29,28 @@ contract dBank {
   }
 
   function withdraw() public {
-    //check if msg.sender deposit status is true
-    //assign msg.sender ether deposit balance to variable for event
+    require(isDeposited[msg.sender] == true, 'no previous deposit');
+    uint userBalance = etherBalanceOf[msg.sender];
 
-    //check user's hodl time
+    uint depositTime = block.timestamp - depositStart[msg.sender];
 
-    //calc interest per second
-    //calc accrued interest
+    //31668017 - interest(10% APY) per second for min. deposit amount (0.01 ETH), cuz:
+    //1e15(10% of 0.01 ETH) / 31577600 (seconds in 365.25 days)
 
-    //send eth to user
-    //send interest in tokens to user
+    //(etherBalanceOf[msg.sender] / 1e16) - calc. how much higher interest will be (based on deposit), e.g.:
+    //for min. deposit (0.01 ETH), (etherBalanceOf[msg.sender] / 1e16) = 1 (the same, 31668017/s)
+    //for deposit 0.02 ETH, (etherBalanceOf[msg.sender] / 1e16) = 2 (doubled, (2*31668017)/s)
+    uint interestPerSecond = 31668017 * (etherBalanceOf[msg.sender] / 1e16);
+    uint interest = interestPerSecond * depositTime;
 
-    //reset depositer data
+    msg.sender.transfer(userBalance);
+    token.mint(msg.sender, interest);
 
-    //emit event
+    etherBalanceOf[msg.sender] = 0;
+    depositStart[msg.sender] = 0;
+    isDeposited[msg.sender] = false;
+
+    emit Withdraw(msg.sender, userBalance, depositTime, interest);
   }
 
   function borrow() payable public {
